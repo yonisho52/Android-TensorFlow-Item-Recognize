f9bb4dd61471b49766c14c62b8a128904a8dde9f
yonisho52 <yonisho52@gmail.com>
2021-04-16 12:21:47 +0300

    initialize

---
+++ app/src/main/java/com/example/tensorflow_item_recognize/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.tensorflow_item_recognize;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
